2633d0fa82c65c34e3af6e68b6eb0f7409262c4f
GamithaChamoda <gamithachamoda.official@gmail.com>
2025-03-10 00:09:32 +0530

    structure and some of home page

---
+++ home.js
@@ -0,0 +1,33 @@
+function undergraduate1(){
+    document.getElementById("underAs").style.display="block";
+    document.getElementById("underUndergraduate").style.display="block";
+    document.getElementById("me").src="images/home/home body/gamitha(4).png";
+}
+function undergraduate2(){
+    document.getElementById("underAs").style.display="none";
+    document.getElementById("underUndergraduate").style.display="none";
+    document.getElementById("me").src="images/home/home body/gamitha(1).png";
+}
+
+function inventor1(){
+    document.getElementById("inventorAs").style.display="block";
+    document.getElementById("inventorInventor").style.display="block";
+    document.getElementById("me").src="images/home/home body/gamitha(2).png";
+}
+function inventor2(){
+    document.getElementById("inventorAs").style.display="none";
+    document.getElementById("inventorInventor").style.display="none";
+    document.getElementById("me").src="images/home/home body/gamitha(1).png";
+}
+
+function leader1(){
+    document.getElementById("LeaderAs").style.display="block";
+    document.getElementById("Leader").style.display="block";
+    document.getElementById("me").src="images/home/home body/gamitha(3).png";
+}
+function leader2(){
+    document.getElementById("LeaderAs").style.display="none";
+    document.getElementById("Leader").style.display="none";
+    document.getElementById("me").src="images/home/home body/gamitha(1).png";
+}
+
