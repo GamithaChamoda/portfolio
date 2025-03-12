570cb577eb6efb244abde51322bcee323b863df5
GamithaChamoda <gamithachamoda.official@gmail.com>
2025-03-12 10:42:26 +0530

    added videos to home and sound to leader mode

--- home.js
+++ home.js
@@ -1,32 +1,68 @@
 function undergraduate1(){
+    document.getElementById("bgVideo").src="videos/home/video(3).mp4";
+    document.getElementById("hi").style.display="none";
+    document.getElementById("im").style.display="none";
+    document.getElementById("gami").style.display="none";
+    document.getElementById("chamo").style.display="none";
     document.getElementById("underAs").style.display="block";
     document.getElementById("underUndergraduate").style.display="block";
     document.getElementById("me").src="images/home/home body/gamitha(4).png";
 }
 function undergraduate2(){
+    document.getElementById("bgVideo").src="videos/home/video(1).mp4";
+    document.getElementById("hi").style.display="block";
+    document.getElementById("im").style.display="block";
+    document.getElementById("gami").style.display="block";
+    document.getElementById("chamo").style.display="block";
     document.getElementById("underAs").style.display="none";
     document.getElementById("underUndergraduate").style.display="none";
     document.getElementById("me").src="images/home/home body/gamitha(1).png";
 }
 
 function inventor1(){
+    document.getElementById("bgVideo").src="videos/home/video(2).mp4";
+    document.getElementById("hi").style.display="none";
+    document.getElementById("im").style.display="none";
+    document.getElementById("gami").style.display="none";
+    document.getElementById("chamo").style.display="none";
     document.getElementById("inventorAs").style.display="block";
     document.getElementById("inventorInventor").style.display="block";
     document.getElementById("me").src="images/home/home body/gamitha(2).png";
 }
 function inventor2(){
+    document.getElementById("bgVideo").src="videos/home/video(1).mp4";
+    document.getElementById("hi").style.display="block";
+    document.getElementById("im").style.display="block";
+    document.getElementById("gami").style.display="block";
+    document.getElementById("chamo").style.display="block";
     document.getElementById("inventorAs").style.display="none";
     document.getElementById("inventorInventor").style.display="none";
     document.getElementById("me").src="images/home/home body/gamitha(1).png";
 }
 
 function leader1(){
+    document.getElementById("bgVideo").src="videos/home/video(4).mp4";
+    document.getElementById("fireAudio").src="audio/home/fireSound.mp3";
+    document.getElementById("hi").style.display="none";
+    document.getElementById("im").style.display="none";
+    document.getElementById("gami").style.display="none";
+    document.getElementById("chamo").style.display="none";
     document.getElementById("LeaderAs").style.display="block";
     document.getElementById("Leader").style.display="block";
     document.getElementById("me").src="images/home/home body/gamitha(3).png";
+    document.getElementById("fireLeft").style.display="block";
+    document.getElementById("fireRight").style.display="block";
 }
 function leader2(){
+    document.getElementById("bgVideo").src="videos/home/video(1).mp4";
+    document.getElementById("fireAudio").src="no";
+    document.getElementById("hi").style.display="block";
+    document.getElementById("im").style.display="block";
+    document.getElementById("gami").style.display="block";
+    document.getElementById("chamo").style.display="block";
     document.getElementById("LeaderAs").style.display="none";
     document.getElementById("Leader").style.display="none";
     document.getElementById("me").src="images/home/home body/gamitha(1).png";
+    document.getElementById("fireLeft").style.display="none";
+    document.getElementById("fireRight").style.display="none";
 }
